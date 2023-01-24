b944b9d6d7cee2471a44662db28c99946a48ead1
chrismorningstaa <christianitymendoza2828@gmail.com>
2023-01-24 17:23:12 +0800

    Add project files.

---
+++ Tano1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Tano1
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+            List<int> numbers = new List<int>();
+            Console.Write("Input : ");
+            int[] input = new int[int.Parse(Console.ReadLine())];
+            int mid = input.Length / 2;
+            Console.Write("First Elimination Remains : ");
+            for (int x = 1; x <= input.Length; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    Console.Write("\t" + x);
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Winner : ");
+            for (int x = 1; x <= input.Length; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    numbers.Add(x);
+                }
+            }
+            var length = numbers.Count();
+            Console.Write(numbers[length / 2]);
+		}
+    }
+}
